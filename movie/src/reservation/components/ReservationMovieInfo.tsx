bfb346be701ed13ccb95b9dc5b7c1e1f6d6cc5b3
choiyoungjin <cyjin0507@gmail.com>
2023-11-01 15:47:22 +0900

    reservation page movie poster change

--- movie/src/reservation/components/ReservationMovieInfo.tsx
+++ movie/src/reservation/components/ReservationMovieInfo.tsx
@@ -1,16 +1,17 @@
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { appViewAtom, selectedMovieInfo } from "../../util/recoils/utilRecoil";
 import styled from "styled-components";
-import { APP_VIEW } from "../../constants";
+import { APP_VIEW, POSTER_IMAGE_URL } from "../../constants";
 import MakeReservation from "./MakeReservation";
 
 export default function ReservationMovieInfo() {
     const movieInfo = useRecoilValue(selectedMovieInfo);
     const setAppView = useSetRecoilState(appViewAtom);
+    const posterURL = POSTER_IMAGE_URL[parseInt(movieInfo?.rank ?? "0") -1].url;
 
 
     return <MovieInfoContainer>
-        <img src="https://via.placeholder.com/160x200" alt="" />
+        <img src={posterURL} alt="" style={{width:"160px", borderRadius:"5px"}}/>
         <h2>{movieInfo?.movieNm}</h2>
         <p>{movieInfo?.openDt}</p>
         <button onClick={()=> setAppView(APP_VIEW.movieIntro)}>이전</button>
