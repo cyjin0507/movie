a85d5e2579ead03f33b83b8ab4f7f71f2421d0b1
choiyoungjin <cyjin0507@gmail.com>
2023-11-06 10:58:36 +0900

    new api type define

--- movie/src/types.ts
+++ movie/src/types.ts
@@ -69,3 +69,160 @@ export interface movieCommentList {
 export interface movieScoreList {
     score : number
 }
+
+//
+
+export interface movieList {
+    Query : string;
+    KMAQuery : string;
+    TotalCount : number;
+    Data : movieListData[];
+}
+
+export interface movieListData {
+    CollName : string;
+    TotalCount : number;
+    Count : number;
+    Result : movieListResult[];
+}
+
+export interface movieListResult {
+    DOCID : string;
+    movieId : string;
+    movieSeq : string;
+    title : string;
+    titleEng : string;
+    titleOrg : string;
+    titleEtc : string;
+    prodYear : string;
+    directors : movieDirectorsList;
+    actors : movieActorsList;
+    nation : string;
+    company : string;
+    plots : moviePlotsList;
+    runtime : string;
+    rating : string;
+    genre : string;
+    kmdbUrl : string;
+    type : string;
+    use : string;
+    episodes : string;
+    ratedYn : string;
+    repRatDate : string;
+    repRlsDate : string;
+    ratings : movieRatingsList;
+    keywords : string;
+    posters : string;
+    stlls : string;
+    staffs : movieStaffsList;
+    vods : movieVodsList;
+    openThtr : string;
+    stat : movieStatList[];
+    screenArea : string;
+    screenCnt : string;
+    salesAcc : string;
+    audiAcc : string;
+    statSouce : string;
+    statDate : string;
+    themeSong : string;
+    soundtrack : string;
+    fLocation : string;
+    Awards1 : string;
+    Awards2 : string;
+    regDate : string;
+    modDate : string;
+    Codes : movieCodesList;
+    CommCodes : movieCommCodesList;
+    ALIAS : string;
+}
+
+export interface movieDirectorsList {
+    director : movieDirector[];
+}
+
+export interface movieDirector {
+    directorNm : string;
+    directorEnNm : string;
+    directorId : string;
+}
+
+export interface movieActorsList {
+    actor : movieActor[];
+}
+
+export interface movieActor {
+    actorNm : string;
+    actorEnNm : string;
+    actorId : string;
+}
+
+export interface moviePlotsList {
+    plot : moviePlot[];
+}
+
+export interface moviePlot {
+    plotLang : string;
+    plotText : string;
+}
+
+export interface movieRatingsList {
+    rating : movieRating[];
+}
+
+export interface movieRating {
+    ratingMain : string;
+    ratingDate : string;
+    ratingNo : string;
+    ratingGrade : string;
+    releaseDate : string;
+    runtime : string;
+}
+
+export interface movieStaffsList {
+    staff : movieStaff[];
+}
+
+export interface movieStaff {
+    staffNm : string;
+    staffEnNm : string;
+    staffRoleGroup : string;
+    staffRole : string;
+    staffEtc : string;
+    staffId : string;
+}
+
+export interface movieVodsList {
+    vod : movieVod[];
+}
+
+export interface movieVod {
+    vodClass : string;
+    vodUrl : string;
+}
+
+export interface movieStatList {
+    screenArea : string;
+    screenCnt : string;
+    salesAcc : string;
+    audiAcc : string;
+    statSouce : string;
+    statDate : string;
+}
+
+export interface movieCodesList {
+    Code : movieCode[];
+}
+
+export interface movieCode {
+    CodeNm : string;
+    CodeNo : string;
+}
+
+export interface movieCommCodesList {
+    CommCode : movieCommCode[];
+}
+
+export interface movieCommCode {
+    CodeNm : string;
+    CodeNo : string;
+}
--- movie/src/util/api.ts
+++ movie/src/util/api.ts
@@ -1,7 +1,17 @@
-import { boxOffice } from "../types";
+import { boxOffice, movieList } from "../types";
 
 export async function getBoxOfficeList(targetDate:string) : Promise<boxOffice> {
     const api = await fetch(`https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=${targetDate}`);
     const parse = await api.json();
+    return parse;
+}
+
+const SERVICE_KEY = "DYX6FDA6T09FXA33Q8N9";
+
+export async function getMovieList() : Promise<movieList> {
+    const api = await fetch(`https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&releaseDts=20231105&ServiceKey=${SERVICE_KEY}`);
+    const parse = await api.json();
+    console.log(parse);
+    
     return parse;
 }
